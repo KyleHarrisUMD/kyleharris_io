ac7b666cc3a9afa4958f5472bd99bb93d611aabb
Kyle Harris <kyhar@umd.edu>
2016-05-04 00:09:59 -0400

    "tweaked css settings. temps loading from static"

--- siteroot/contact/models.py
+++ siteroot/contact/models.py
@@ -3,3 +3,14 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    name  = models.CharField(max_length=36)
+    email = models.EmailField()
+    message_text = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return_string = "\nName: "+self.name + '\n' + "Email:"+self.email \
+                        + '\n' + "Message :" +self.message_text+"\n\n"
+        return return_string
+
--- siteroot/contact/views.py
+++ siteroot/contact/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from .import forms
+from .models import Message
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
 
 # the view is returning data via a post request,
 # so make it pay attention to the request method.
@@ -14,4 +20,21 @@ def contact_view(request):
         # check if the form is valid.
         if form.is_valid():
             print("Good form")
+            print("Data dump :")
+
+            # access data with form.cleaned_data['key_name']
+            name  = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message_text = form.cleaned_data['message_text']
+
+            # now put the data into the database
+            db_entry = Message()
+            db_entry.name = name
+            db_entry.email = email
+            db_entry.message_text = message_text
+
+            # save the message
+            db_entry.save()
+
+
     return render(request, 'contact/contact_form.html', {'form':form})
